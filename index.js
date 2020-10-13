553cdc4bf4e60bed3bf349941b8b13ccf3a1ffed
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-10-13 13:38:33 -0400

    Made minor changes to syntax in some JSON responses

--- index.js
+++ index.js
@@ -40,16 +40,16 @@ app.get('/movies', passport.authenticate('jwt', { session: false}), (req, res) =
 });
 
 // Get a list of Users
-app.get('/users', passport.authenticate('jwt', { session: false}), (req, res) => {
-  Users.find()
-  .then((users) => {
-    res.status(201).json(users);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send('Error: ' + err);
-  });
-});
+// app.get('/users', passport.authenticate('jwt', { session: false}), (req, res) => {
+//   Users.find()
+//   .then((users) => {
+//     res.status(201).json(users);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     res.status(500).send('Error: ' + err);
+//   });
+// });
 
 // Get data about a single movie, by title
 app.get('/movies/:Title', passport.authenticate('jwt', { session: false}), (req, res) => {
@@ -67,7 +67,7 @@ app.get('/movies/:Title', passport.authenticate('jwt', { session: false}), (req,
 app.get('/movies/Genres/:Title', passport.authenticate('jwt', { session: false}), (req, res) => {
   Movies.findOne({ Title : req.params.Title})
     .then((movie) => {
-      res.status(201).json(movie.Genre.Name + ". " + movie.Genre.Description);
+      res.status(201).json("Genre: "+ movie.Genre.Name + ". Descripton: " + movie.Genre.Description);
  })
    .catch((err) => {
      console.error(err);
@@ -79,7 +79,7 @@ app.get('/movies/Genres/:Title', passport.authenticate('jwt', { session: false})
 app.get('/movies/Directors/:Name', passport.authenticate('jwt', { session: false}), (req, res) => {
   Movies.findOne({ "Director.Name" : req.params.Name})
     .then((movie) => {
-      res.status(201).json(movie.Director.Name + ': ' + movie.Director.Bio);
+      res.status(201).json("Name: "+ movie.Director.Name + ". Bio: " + movie.Director.Bio + " Birth: " + movie.Director.Birth + ". Death: " + movie.Director.Death + ".");
  })
    .catch((err) => {
      console.error(err);
@@ -116,16 +116,16 @@ app.post('/users', passport.authenticate('jwt', { session: false}), (req, res) =
 });
 
 //Get data about a single user by Username
-app.get('/users/:Username', passport.authenticate('jwt', { session: false}), (req, res) => {
-  Users.findOne({ Username: req.params.Username })
-    .then((user) => {
-      res.json(user);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send('Error: ' + err);
-  });
-});
+// app.get('/users/:Username', passport.authenticate('jwt', { session: false}), (req, res) => {
+//   Users.findOne({ Username: req.params.Username })
+//     .then((user) => {
+//       res.json(user);
+//   })
+//   .catch((err) => {
+//     console.error(err);
+//     res.status(500).send('Error: ' + err);
+//   });
+// });
 
 // Put updates to user information
 app.put('/users/:Username', passport.authenticate('jwt', { session: false}), (req, res) => {
@@ -191,7 +191,7 @@ app.delete('/users/:Username', passport.authenticate('jwt', { session: false}), 
      if (!user) {
        res.status(400).send(req.params.Username + ' was not found.');
   } else {
-        res.status(200).send(req.params.Username + ' was deleted.');
+        res.status(200).send(req.params.Username + ' was successfully deregistered from myFlix.');
   }
 })
   .catch((err) => {
