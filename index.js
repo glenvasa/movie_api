98ba5c75e29105bf8147b3fdec2bb878bcf7c567
glenvasa <70590043+glenvasa@users.noreply.github.com>
2020-10-28 15:15:05 -0400

    Enable jwt authentication for movies endpoint

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.get('/', (req, res) => {
 });
 
 // Get a list of data about all movies
-app.get('/movies', (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false}), (req, res) => {
   Movies.find()
   .then((movies) => {
     res.status(201).json(movies);
