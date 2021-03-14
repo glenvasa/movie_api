c8ba90bc9726ea66e42b5168b98e06d8c0255714
glenvasa <70590043+glenvasa@users.noreply.github.com>
2021-03-14 17:12:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234",
   "http://localhost:4200",
+  "http://localhost:3000",
   "https://myflix2020.netlify.app",
   "https://glenvasa.github.io",
 ];
